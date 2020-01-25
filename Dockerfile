faca741c9d63f5409a05f25b24515a10e899caa2
alvarepc <55838813+alvarepc@users.noreply.github.com>
2020-01-25 16:43:03 +0800

    Initial files

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM 			docker.io/centos
+MAINTAINER 		demo "demo@example.com"
+USER 			root
+EXPOSE 			3000
+COPY 			app.js package.json /
+RUN				yum update -y \
+				&& yum install nodejs npm -y \
+				&& yum clean all \
+				&& npm install
+CMD				["bash", "-c", "node app.js"]
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+
+var route = express.Router();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!\n')
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!\n')
+});
+
+app.use('/', route);
+
+// Start defining routes for our app/microservice
+// A route that dumps hostname information from pod
+route.get('/', function(req, res) {
+        res.send('Hi! I am running on host ->' + hostname + '\n');
+});
+
+// A route used for the readiness probe in openshift
+route.get('/ready', function(req, res){
+        res.send('READY\n');
+});
+
+// A route used for health check in openshift
+route.get('/health', function(req, res){
+        res.send('OK\n');
+});
