8c9198289fa019dc7351396465a5a21dc6088969
alvarepc <55838813+alvarepc@users.noreply.github.com>
2020-02-01 19:15:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!\n')
 });
 
+app.get('/ready', (req, res) => {
+  res.send('READY\n');
+});
+
+app.get('/health', (req, res) => {
+  res.send('OK\n');
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!\n')
 });
